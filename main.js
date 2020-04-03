b0820ddfd1bc5b5ccdb7d473311cfba662e3609d
Maciek <maciej.gorski995@gmail.com>
2020-04-03 22:36:48 +0200

    Fix bugs, code maintain

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 window.onload = () => {
-    let coins = 500
+    let coins = 10
     let userInput = 1
-
-    const gameBoard = document.querySelector("#gameboard")
-    let offsetX, offsetY
     
-    gameBoard.addEventListener('mousemove', (e) => {
+    const gameBoard = document.querySelector("#gameboard")
+    let offsetX, offsetY // Declare global values
+
+    document.querySelector("#coins-value").innerHTML = coins // Assign value 'coins' to HTML element with id of #coins-value.
+    
+    // Get mouse position
+    gameBoard.addEventListener('mousemove', (e) => { 
         offsetX = e.offsetX
         offsetY = e.offsetY
     })
     
-    const randomPosition = () => {
+    // Use to calculate random y position of a block
+    const randomPosition = () => { 
         let random = Math.floor(Math.random() * 3);
         let y = [132.08346, 177.08346, 222.08346]
 
         return y[random];
     }
     
+    // Generate new block
     const cubeGenerator = (y) => {
         let cube = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
         let svg = document.querySelector('html > body > div > svg')
@@ -32,54 +37,47 @@ window.onload = () => {
         cube.setAttribute("height", "45.83309")
         
         svg.appendChild(cube)
-        return cube
+        return cube // Return that is used to gather block's attributes
     }  
 
-    document.querySelector("#coins-value").innerHTML = coins
     
     gameBoard.addEventListener('click', () => {
-        const userInput = document.querySelector("#bet").value
-        let multiplier = document.querySelector("#multiplier").value
-        document.querySelector("#coins-value").innerHTML = coins
+        const userInput = document.querySelector("#bet").value // Get user input
+        let multiplier = document.querySelector("#multiplier").value // Get multiplier value
+        document.querySelector("#coins-value").innerHTML = coins // Alter assigned value to HTML element
         
+        // Game engine,
         const race = () => {
-            let i = 580;
+            let i = 580 // iterator
             const intervalId = setInterval(() => {
                 document.querySelector("#svg_4").x.baseVal.value = i-= 10;
 
-                if (i < 4) {
+                if (i < 10) {
                     let yVal = document.querySelector("#svg_4").y.baseVal.value;
-                    //console.log(offsetY, yVal)
-                    
-                    if (offsetY > (yVal - 45) && offsetY < (yVal + 45)) {                          
+                    if (offsetY < (yVal + 45) && offsetY > yVal) { 
                         if (userInput > coins) {
                             alert("You don't have enough coins!")
-                        } else if (userInput != 0 && userInput < coins) {
-                            coins = coins + ( userInput * multiplier )
+                        } else if (userInput != 0 && userInput <= coins) {
+                            coins = coins + (userInput * multiplier)
                         }
-                        console.log(offsetY, yVal, coins)
                     } else {
-                        if (userInput != 0 && userInput < coins) {
-                            coins = coins - ( userInput * multiplier )
+                        if (userInput != 0 && userInput <= coins) {
+                            coins = coins - (userInput * multiplier)
                         } else if (userInput > coins) {
                             alert("You don't have enough coins!")
                         }
                     }
-                    clearInterval(intervalId)
+                    clearInterval(intervalId) 
                 }
-            }, 20);
+            }, 1);
         }
         
-
         let parent = document.querySelector('html > body > div > svg');
         let child = document.querySelector("#svg_4")
-
-        cubeGenerator(randomPosition())
+        
+        cubeGenerator(randomPosition()) // Call cubeGenerator with randomPosition() as it's argument.
         race()
-
-        parent.removeChild(child)
-
-
-        console.log(multiplier)    
-    })
+        
+        parent.removeChild(child) // Remove last generated cube
+    }) 
 }
